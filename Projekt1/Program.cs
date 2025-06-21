171623e36b5841a7d67dbc40088705df956884b2
Soemzx <kuba091103@gmail.com>
2025-06-21 20:55:55 +0200

    Champion

--- Projekt1/Program.cs
+++ Projekt1/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Champion
+{
+    public string name { get; }
+    public int age { get; set; }
+    private string location;
+
+    private List<string> locationHistory;
+    public int health { get; private set; }
+    public int attack { get; }
+    public Champion(string _name, int age, string location, int health, int attack)
+    {
+        name = _name;
+        this.age = age;
+        this.location = location;
+        this.locationHistory = new List<string>();
+        this.health = health;
+        this.attack = attack;
+    }
+    public string GetLocation() { return location; }
+    public void SetLocation(string location)
+    {
+        locationHistory.Add(this.location);
+        this.location = location;
+    }
+    public void ShowLocationHistory()
+    {
+        foreach (string location in locationHistory)
+        {
+            Console.WriteLine("////////////////");
+            Console.WriteLine(location);
+            Console.WriteLine("////////////////");
+        }
+    }
+    public void GetDamaged(int damage)
+    {
+        health = health - damage;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Champion katarina = new Champion("Katarina", 16, "Noxus", 100, 45);
+        Champion Garen = new Champion("Garen", 25, "Demacia", 200, 35);
+        Console.WriteLine(katarina.name);
+        Console.WriteLine(katarina.age);
+
+        katarina.age = 17;
+        Console.WriteLine(katarina.age);
+        Console.WriteLine(katarina.GetLocation());
+        katarina.ShowLocationHistory();   
+        katarina.SetLocation("Demacia");
+        katarina.ShowLocationHistory();       
+        katarina.SetLocation("Ionia");
+        katarina.ShowLocationHistory();
+        katarina.GetDamaged(Garen.attack);
+    }
+}
--- Projekt1/obj/Debug/net8.0/Projekt1.AssemblyInfo.cs
+++ Projekt1/obj/Debug/net8.0/Projekt1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2df600fbd8588ec90a5082ae8a108d95e90eec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
